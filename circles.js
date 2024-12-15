732bb16c918993bc4e1f8fe3f646a9fe80f34e2e
mylabernard8 <myla.bernard26@avenues.org>
2024-12-15 18:11:18 -0500

    Skeletal code or recursive circles (but instead using squares)

--- circles.js
+++ circles.js
@@ -1,1 +1,20 @@
+function setup() {
+  createCanvas(400, 400); 
+  noFill();               
+  noLoop();               
+}
 
+function draw() {
+  background(220);        
+  rSquare(x, y, size); //recursive code (have to finish code later)
+}
+
+function rSquare(x, y, size) {
+  if (/* stop recursive function */) {
+    return;
+  }
+
+  // draw in square (have to flush out later)
+
+  // make recursive formula for smaller shapes
+}
