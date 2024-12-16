92d544e7f5b0f69a9d701839a2a26f63cece3e67
mylabernard8 <myla.bernard26@avenues.org>
2024-12-16 10:34:47 -0500

    Made some tweaks to code to make sure no mistakes

--- circles.js
+++ circles.js
@@ -1,7 +1,3 @@
-// recursive drawing of squares along both x and y axes
-// base case: Stop recursion when the circle's radius is below a certain size
-// recursive case: draw smaller squares at specific offsets from the current square
-
 function setup() {
   createCanvas(400, 400); // Set up the canvas size
   noFill();  // don't fill in the shapes 
@@ -14,7 +10,7 @@ function draw() {
 }
 
 function rSquare(x, y, size) {
-  if (size < 4) { // base case:recursive code (stopping when squares get too small)
+  if (size < 4) { // //recursive code (stopping when squares get too small)
     return;
   }
 
@@ -24,7 +20,7 @@ function rSquare(x, y, size) {
   // draw smaller squares
   const newSize = size / 2;
 
-  // recursive case: recursive formula for each corner of the squares
+  // recursive formula for each corner of the squares
   rSquare(x - newSize, y - newSize, newSize); // Top-left
   rSquare(x + newSize, y - newSize, newSize); // Top-right
   rSquare(x - newSize, y + newSize, newSize); // Bottom-left
